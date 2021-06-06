e36d84e717c8d92b36bcb9e4928491d9a65b8195
Monique <monique.simberg@mohawkcollege.ca>
2021-06-06 19:53:12 -0400

    step 2

--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -7,3 +7,6 @@ import prime
 def test_generate_prime_factors():
     """ Pass when not Int """
     assert prime.generate_prime_factors("16") == []
+def test_2_generate_prime_factors():
+    """ empty list """
+    assert prime.generate_prime_factors(1) == []
