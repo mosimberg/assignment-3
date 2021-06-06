2607ecd81acd0558343a244bb3ef70c40a639af1
Monique <monique.simberg@mohawkcollege.ca>
2021-06-06 19:51:16 -0400

    step one

--- prime.py
+++ prime.py
@@ -1,3 +1,10 @@
 """
 prime.py -- Write the application code here
 """
+
+def generate_prime_factors(n):
+    if isinstance(n, int):
+        factors = []
+        return factors
+    else:
+        return []
--- tests/test_generate_prime_factors.py
+++ tests/test_generate_prime_factors.py
@@ -2,3 +2,8 @@
 """
 The test module for Prime Factors
 """
+import prime
+
+def test_generate_prime_factors():
+    """ Pass when not Int """
+    assert prime.generate_prime_factors("16") == []
